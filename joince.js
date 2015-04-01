d8e4ec9ae78960544038338c8b2248f8f7ba639d
KeyboardFire <andy@keyboardfire.com>
2015-04-01 16:11:27 -0500

    cleaned up joystick code

--- joince.js
+++ joince.js
@@ -13,7 +13,63 @@ var joince = {
     pos: { x: 0, y: 0 }, dim: { w: scale(25), h: scale(25) },
     move: { left: false, up: false, right: false, down: false },
     consts: { SPEED: scale(4), joystick: { NUB_RADIUS: scale(12), OUTER_RADIUS: scale(36) } },
-    joystick: { create: joystickCreate, destroy: joystickDestroy, nub: null, pos: null }
+    joystick: {
+        create: function(pos) {
+            joince.joystick.nub = joince.joystick.pos = pos;
+        }, destroy: function() {
+            joince.joystick.nub = joince.joystick.pos = null;
+            joince.move.left = joince.move.right = joince.move.up = joince.move.down = false;
+        }, update: function(pos) {
+            if (pos) {
+                joince.joystick.nub = pos;
+
+                var dx = joince.joystick.pos.x - joince.joystick.nub.x,
+                    dy = joince.joystick.pos.y - joince.joystick.nub.y;
+
+                if (Math.sqrt(dx*dx + dy*dy) > joince.consts.joystick.OUTER_RADIUS) {
+                    var angle = Math.atan2(-dx, -dy);
+
+                    joince.joystick.nub = {
+                        x: joince.joystick.pos.x + Math.sin(angle) * joince.consts.joystick.OUTER_RADIUS,
+                        y: joince.joystick.pos.y + Math.cos(angle) * joince.consts.joystick.OUTER_RADIUS
+                    };
+                }
+            } else {
+                if (joince.joystick.nub) {
+                    joince.ctx.beginPath();
+                    joince.ctx.arc(joince.joystick.nub.x,
+                                   joince.joystick.nub.y,
+                                   joince.consts.joystick.NUB_RADIUS,
+                                   0, Math.PI*2);
+                    joince.ctx.fill();
+
+                    joince.ctx.beginPath();
+                    joince.ctx.arc(joince.joystick.pos.x,
+                                   joince.joystick.pos.y,
+                                   joince.consts.joystick.OUTER_RADIUS,
+                                   0, Math.PI*2);
+                    joince.ctx.stroke();
+
+                    var dx = joince.joystick.pos.x - joince.joystick.nub.x,
+                        dy = joince.joystick.pos.y - joince.joystick.nub.y;
+
+                    if (Math.abs(dx) >= joince.consts.joystick.OUTER_RADIUS / 2) {
+                        joince.move.left = dx > 0;
+                        joince.move.right = dx < 0;
+                    } else {
+                        joince.move.left = joince.move.right = false;
+                    }
+
+                    if (Math.abs(dy) >= joince.consts.joystick.OUTER_RADIUS / 2) {
+                        joince.move.up = dy > 0;
+                        joince.move.down = dy < 0;
+                    } else {
+                        joince.move.up = joince.move.down = false;
+                    }
+                }
+            }
+        }, nub: null, pos: null
+    }
 }
 
 window.addEventListener('load', function() {
@@ -43,24 +99,10 @@ window.addEventListener('load', function() {
     var mouseListen = function(pressed, mouse) {
         return pressed ? function(e) {
             e.preventDefault();
-
             joince.joystick.create(getClickPos(e));
             window.addEventListener(mouse ? 'mousemove' : 'touchmove', moveListener = function(e) {
                 e.preventDefault();
-
-                joince.joystick.nub = getClickPos(e);
-
-                var dx = joince.joystick.pos.x - joince.joystick.nub.x,
-                    dy = joince.joystick.pos.y - joince.joystick.nub.y;
-
-                if (Math.sqrt(dx*dx + dy*dy) > joince.consts.joystick.OUTER_RADIUS) {
-                    var angle = Math.atan2(-dx, -dy);
-
-                    joince.joystick.nub = {
-                        x: joince.joystick.pos.x + Math.sin(angle) * joince.consts.joystick.OUTER_RADIUS,
-                        y: joince.joystick.pos.y + Math.cos(angle) * joince.consts.joystick.OUTER_RADIUS
-                    };
-                }
+                joince.joystick.update(getClickPos(e));
             });
         } : function(e) {
             joince.joystick.destroy();
@@ -80,36 +122,8 @@ window.addEventListener('load', function() {
         joince.pos.y += (joince.move.up ? -joince.consts.SPEED : 0)
                       + (joince.move.down ? joince.consts.SPEED : 0);
 
-        if (joince.joystick.nub) {
-            joince.ctx.beginPath();
-            joince.ctx.arc(joince.joystick.nub.x,
-                           joince.joystick.nub.y,
-                           joince.consts.joystick.NUB_RADIUS,
-                           0, Math.PI*2);
-            joince.ctx.fill();
-
-            joince.ctx.beginPath();
-            joince.ctx.arc(joince.joystick.pos.x,
-                           joince.joystick.pos.y,
-                           joince.consts.joystick.OUTER_RADIUS,
-                           0, Math.PI*2);
-            joince.ctx.stroke();
-
-            var dx = joince.joystick.pos.x - joince.joystick.nub.x,
-                dy = joince.joystick.pos.y - joince.joystick.nub.y;
-
-            if (Math.abs(dx) < joince.consts.joystick.OUTER_RADIUS / 2) {}
-            else if (dx < 0) joince.pos.x += joince.consts.SPEED;
-            else if (dx > 0) joince.pos.x -= joince.consts.SPEED;
-
-            if (Math.abs(dy) < joince.consts.joystick.OUTER_RADIUS / 2) {}
-            else if (dy < 0) joince.pos.y += joince.consts.SPEED;
-            else if (dy > 0) joince.pos.y -= joince.consts.SPEED;
-        }
+        joince.joystick.update();
 
         joince.ctx.fillRect(joince.pos.x, joince.pos.y, joince.dim.w, joince.dim.h);
     }, 20);
 });
-
-function joystickCreate(pos) { joince.joystick.nub = joince.joystick.pos = pos; }
-function joystickDestroy() { joince.joystick.nub = joince.joystick.pos = null; }
